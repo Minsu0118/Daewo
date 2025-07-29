6eb242e676fdf8353e0f6090003b6f181bc5f41d
박민수 <50141291+Minsu0118@users.noreply.github.com>
2025-07-30 05:05:59 +0900

    Delete src directory

--- src/main/java/repository/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Member;
-import org.apache.ibatis.annotations.Mapper;
-
-public interface MemberMapper {
-    void insert(Member member);
-    Member findByUsername(String username);
-}
--- src/main/java/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import model.Member;
-
-public interface MemberService {
-    void register(Member member);
-    Member getByUsername(String username);
-}
